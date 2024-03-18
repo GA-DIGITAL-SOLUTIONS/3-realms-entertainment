9df2ba3b13c522ff9786f61c04b27075315b40eb
Satya2553 <mareedu..satyanarayana.5475@gmail.com>
2024-03-18 11:23:27 +0530

    initial setup

--- movie_booking_app/settings.py
+++ movie_booking_app/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-absrw1u29sz9367kik$)&(oz3)(twa4uoeni!ej4q!a_6z4zou'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['three-realms-entertainment.onrender.com']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
